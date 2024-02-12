526527e7f6d27332fed9868aacb7742153192f04
DanielHughes1580 <rayman_dan@hotmail.co.uk>
2024-02-12 20:59:22 +0000

    Legend added and markers altered on map

--- Chloropleth_files/logic.js
+++ Chloropleth_files/logic.js
@@ -16,13 +16,6 @@ let url = 'https://raw.githubusercontent.com/ERAA1997/Project-3-UoB--Glasgow-Hou
 // Fetch GeoJSON data
 d3.json(url).then(function(data) {
     
-    // data.features.forEach(function (feature) {
-    //     var coordinates = feature.geometry.coordinates[0].map(function (coord) {
-    //         return [coord[1], coord[0]];
-    //     });
-    //     feature.geometry.coordinates[0] = coordinates;
-    // });
-
     // Add GeoJSON layer to Leaflet map
     let geojson = L.choropleth(data, {
 
@@ -46,14 +39,48 @@ d3.json(url).then(function(data) {
 
         // Popup information
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("<b>" + feature.properties.name + "</b><br>" + feature.properties.description);
+            var formattedPercentage = (feature.properties.HA_Percentage).toFixed(2) + '%';
+            layer.bindPopup("<b>Area:" + feature.properties.name + "</b><br> Percentage Owned by Housing Agency:" + formattedPercentage);
         }
     }).addTo(map);
 });
 
-// Function to determine color based on feature index
-function getColor(index) {
-    
-    var colors = ['green', 'blue', 'red', 'orange', 'purple'];  // Example colors
-    return colors[index % colors.length];
+// Legend
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [5, 10, 15, 20, 25, 30, 35, 40, 45],
+        labels = [],
+        from, to;
+
+    // loop through and generate a label with a colour for each interval
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
 };
+
+// Add legend to map
+legend.addTo(map);
+
+// Function to get color based on HA_Percentage
+function getColor(d) {
+    return d > 45 ? '#b10026' :
+           d > 40 ? '#e31a1c' :
+           d > 35 ? '#fc4e2a' :
+           d > 30 ? '#fd8d3c' :
+           d > 25 ? '#feb24c' :
+           d > 20 ? '#fed976' :
+           d > 15 ? '#ffeda0' :
+           d > 10 ? '#fffeda' :
+           d > 5 ? '#ffffb2' :
+                   '#ffffff';
+}
